85c95253019cac437e08374ff54e1ad52bdf1b99
AshokKumarKaruppasamy1989 <vivalaraza1989@gmail.com>
2024-03-22 07:59:04 +0530

    practice 3Mar

--- src/main/java/com/practical/test.java
+++ src/main/java/com/practical/test.java
@@ -15,36 +15,45 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class test {
 
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-		
+
 		driver.get("https://www.expedia.com/");
 		Thread.sleep(5000);
-		
+
 		Actions action = new Actions(driver);
 		action.sendKeys(Keys.ESCAPE).perform();
 		Thread.sleep(3000);
-		
+
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@aria-label='Going to']")));
-		
+
 		driver.findElement(By.xpath("//button[@aria-label='Going to']")).click();
 		
-		driver.findElement(By.id("destination_form_field")).sendKeys("Chennai");
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("destination_form_field")));
+
+//		driver.findElement(By.id("destination_form_field")).sendKeys("Chennai");
+		/* using with out input field */
+		driver.findElement(By
+				.xpath("//div[@class='uitk-field has-no-visible-label has-placeholder']//label//following-sibling::*[1]"))
+				.sendKeys("Chennai");
+
 		Thread.sleep(2000);
-		
+
 		List<WebElement> option = driver.findElements(By.xpath("//div[@class='typeahead-custom-truncate']"));
-		System.out.println(option.size()); 
-		
+		System.out.println(option.size());
+
 		wait.until((ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='typeahead-custom-truncate']"))));
-		
-		for(WebElement o: option) {
-			if(o.getText().equalsIgnoreCase("Chennai (MAA - Chennai Intl.)")) {
+
+		for (WebElement o : option) {
+			if (o.getText().equalsIgnoreCase("Chennai (MAA - Chennai Intl.)")) {
 				System.out.println(o.getText());
+				Thread.sleep(3000);
 				driver.findElement(By.xpath("//button[@aria-label='Chennai (MAA - Chennai Intl.) India']")).click();
-				//o.click();
+//				driver.findElement(By.xpath("//button[@aria-label='" + o.getText() + "']")).click();
+				// o.click();
 				break;
 			}
 		}
